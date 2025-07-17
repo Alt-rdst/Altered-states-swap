6d1f4f3031277032aa73f3943a03292ee1845c3e
Alt-rdst <Alt.rd.st@gmail.com>
2025-07-17 10:47:03 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Altered States Swap</h1>
+      <p>This is your DEX front-end. Let’s wire it up!</p>
+    </div>
+  );
+}
